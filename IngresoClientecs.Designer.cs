b40e788f5feccde7d2857483c2df03fb1090edb4
wendY1g0 <wendy.gonzales@alumnos.ucn.cl>
2024-06-16 15:35:07 -0400

    Test_IC

--- IngresoClientecs.Designer.cs
+++ IngresoClientecs.Designer.cs
@@ -41,41 +41,46 @@
             // nombre
             // 
             this.nombre.AutoSize = true;
-            this.nombre.Location = new System.Drawing.Point(111, 164);
+            this.nombre.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombre.Location = new System.Drawing.Point(80, 153);
             this.nombre.Name = "nombre";
-            this.nombre.Size = new System.Drawing.Size(56, 16);
+            this.nombre.Size = new System.Drawing.Size(99, 27);
             this.nombre.TabIndex = 0;
             this.nombre.Text = "Nombre";
             // 
             // ciudad
             // 
             this.ciudad.AutoSize = true;
-            this.ciudad.Location = new System.Drawing.Point(361, 164);
+            this.ciudad.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ciudad.Location = new System.Drawing.Point(342, 162);
             this.ciudad.Name = "ciudad";
-            this.ciudad.Size = new System.Drawing.Size(50, 16);
+            this.ciudad.Size = new System.Drawing.Size(99, 27);
             this.ciudad.TabIndex = 1;
             this.ciudad.Text = "Ciudad";
             // 
             // tipo
             // 
             this.tipo.AutoSize = true;
-            this.tipo.Location = new System.Drawing.Point(623, 164);
+            this.tipo.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipo.Location = new System.Drawing.Point(602, 162);
             this.tipo.Name = "tipo";
-            this.tipo.Size = new System.Drawing.Size(35, 16);
+            this.tipo.Size = new System.Drawing.Size(59, 27);
             this.tipo.TabIndex = 2;
             this.tipo.Text = "Tipo";
             // 
             // tituloClientes
             // 
             this.tituloClientes.AutoSize = true;
-            this.tituloClientes.Location = new System.Drawing.Point(341, 65);
+            this.tituloClientes.Font = new System.Drawing.Font("Lucida Calligraphy", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tituloClientes.Location = new System.Drawing.Point(260, 32);
             this.tituloClientes.Name = "tituloClientes";
-            this.tituloClientes.Size = new System.Drawing.Size(100, 16);
+            this.tituloClientes.Size = new System.Drawing.Size(272, 36);
             this.tituloClientes.TabIndex = 3;
             this.tituloClientes.Text = "Ingresar Cliente";
             // 
             // textoNombre
             // 
+            this.textoNombre.BackColor = System.Drawing.Color.PeachPuff;
             this.textoNombre.Location = new System.Drawing.Point(66, 192);
             this.textoNombre.Name = "textoNombre";
             this.textoNombre.Size = new System.Drawing.Size(135, 22);
@@ -83,6 +88,7 @@
             // 
             // textoCiudad
             // 
+            this.textoCiudad.BackColor = System.Drawing.Color.PeachPuff;
             this.textoCiudad.Location = new System.Drawing.Point(321, 192);
             this.textoCiudad.Name = "textoCiudad";
             this.textoCiudad.Size = new System.Drawing.Size(135, 22);
@@ -90,26 +96,34 @@
             // 
             // comboTipo
             // 
+            this.comboTipo.BackColor = System.Drawing.Color.PeachPuff;
             this.comboTipo.FormattingEnabled = true;
+            this.comboTipo.Items.AddRange(new object[] {
+            "Normal",
+            "Premium"});
             this.comboTipo.Location = new System.Drawing.Point(573, 192);
             this.comboTipo.Name = "comboTipo";
             this.comboTipo.Size = new System.Drawing.Size(132, 24);
             this.comboTipo.TabIndex = 6;
+            this.comboTipo.SelectedIndexChanged += new System.EventHandler(this.comboTipo_SelectedIndexChanged);
             // 
             // botonEnviar
             // 
-            this.botonEnviar.Location = new System.Drawing.Point(355, 298);
+            this.botonEnviar.BackColor = System.Drawing.Color.SandyBrown;
+            this.botonEnviar.Font = new System.Drawing.Font("Lucida Calligraphy", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonEnviar.Location = new System.Drawing.Point(308, 297);
             this.botonEnviar.Name = "botonEnviar";
-            this.botonEnviar.Size = new System.Drawing.Size(75, 23);
+            this.botonEnviar.Size = new System.Drawing.Size(169, 48);
             this.botonEnviar.TabIndex = 7;
             this.botonEnviar.Text = "Aceptar";
-            this.botonEnviar.UseVisualStyleBackColor = true;
+            this.botonEnviar.UseVisualStyleBackColor = false;
             this.botonEnviar.Click += new System.EventHandler(this.botonEnviar_Click);
             // 
             // IngresoClientecs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.botonEnviar);
             this.Controls.Add(this.comboTipo);
